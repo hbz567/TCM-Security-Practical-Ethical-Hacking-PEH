808dcb5dbe16e6c71a4123dc92397e7d06754463
Muhammad Hasan Bin Zahid <m.hasanbinzahid@gmail.com>
2026-02-21 02:23:06 -0500

    Delete python_programs directory

--- python_programs/Employees.py
+++
@@ -1,9 +0,0 @@
-class Employees:
-	def __init__(self, name, role, status, salary):
-		self.name = name
-		self.role = role
-		self.status = status
-		self.salary = salary
-		
-	def eligible_for_increment(self):
-		return self.salary < 2000;
